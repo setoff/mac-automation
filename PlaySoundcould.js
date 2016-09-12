745fc32f68510317c5a723077fd0dd52c1ad2cdd
Ilya Sedov <ilyasedov@Ilyas-MacBook-Pro-2.local>
2016-09-12 17:31:38 +0500

    Start/stop soundcloud.com player in safari

---
+++ PlaySoundcould.js
@@ -0,0 +1,37 @@
+/**
+  Starts play soundcloud.com player.
+  Considerations:
+  - soundcloud.com has been opened in Safari
+  - soundcloud.com has play controls pane on the page
+    It appears when you click play on any track, but it is absent when site just loaded.
+  How to install:
+  Launch Automator and tap File -> New (this dialog appears on launch)
+    - select docuement type "Service"
+    - add "Run JavaScript" action
+    - paste this script
+    - save
+    This service should appear in any application Services menu.
+    - Tap it to launch
+  Optional: add shortcut to service.
+    - launch System preferencies -> Keyboard -> Shortcuts
+    - select Services, select this service and type needed shortcut.
+    I use ^⎇F8 it is pretty convenient ;)
+*/
+
+
+function run(input, parameters) {
+	Safari = Application('Safari')
+	for (var wndx in Safari.windows) {
+		var window = Safari.windows[wndx]
+		if (window.name() == 'Extensions' || window.name().length == 0 ) continue;
+		sounds = window.tabs().filter(function(item) {
+			return item.url().indexOf('soundcloud.com') != -1
+		})
+		if (sounds.length == 1) {
+			Safari.doJavaScript(
+        'document.getElementsByClassName("playControl")[0].click()',
+        { in: sounds[0] })
+			break
+		}
+	}
+}
